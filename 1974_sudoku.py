ddc66ea691771c15623f3b33e48901dbdd0f632a
jinyoooooooung <oaoa9n@gmail.com>
2023-08-11 12:43:59 +0900

    230811

--- 1974_sudoku.py
+++ 1974_sudoku.py
@@ -1,21 +1,37 @@
 import sys
 sys.stdin = open("1974_input.txt", "r")
 
-T = int(input())
+
 
 def sudoku_f(arr):
-    for i in range(9):
-        cnt = [0]*10
-        # print(tc, cnt)
-        for j in range(9):
-            cnt[arr[i][j]] += 1
-        for k in range(1, 10):
-            if cnt[k] == 0:
-                return 0
-        if sum(cnt) == 9:
-            return 1
+    # 9줄 검사
+    for i in range(1, 9):
+        cnt_h = [0]*10
+        cnt_v = [0]*10
+
+        for j in range(1, 9):
+            # 가로 검사
+            cnt_h[arr[i][j]] += 1
+            # 세로 검사
+            cnt_v[arr[j][i]] += 1
+            # print(tc, cnt_h)
+        if 2 in cnt_h or 2 in cnt_v:
+            return 0
+
+    # 3x3 검사
+    for x in range(0, 9, 3):
+        for y in range(0, 9, 3):
+            pan = [0]*10
+            for i in range(3):
+                for j in range(3):
+                    pan[arr[x+i][y+j]] += 1
+                    if pan[arr[x+i][y+j]] == 2:
+                        # print(pan)
+                        return 0
+    return 1
 
 
+T = int(input())
 for tc in range(1, T+1):
     sudoku = []
     # 스도쿠 배열 완성
