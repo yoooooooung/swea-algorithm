bb9f3fc6bd948f6004e99d11ce66442a6852bb50
jinyoooooooung <oaoa9n@gmail.com>
2023-08-23 11:54:03 +0900

    230823

--- 5177.py
+++ 5177.py
@@ -1,10 +1,41 @@
-# 이진 합
+# 이진 힙
 import sys
 sys.stdin = open("5177_input.txt")
 
 T = int(input())
 for tc in range(1, T+1):
-    N = int(input())
-    arr = [0] + list(map(int, input().split()))
-    print(arr)
-    # 부모가 자식보다 작은 수 여야함. (부모 인덱스 = n//2)
+    N = int(input())    # 노드의 수
+    tree = [0] + list(map(int, input().split()))
+    # print(tree)
+    last = N//2    # 마지막 노드의 부모 번호
+    result = 0
+
+
+    # tree -> 완전 이진 트리 만들기
+    for i in range(1, N+1):
+        while tree[i] < tree[i//2]:        # 내가 부모 노드보다 작으면
+            tree[i], tree[i//2] = tree[i//2], tree[i]   # 자리 바꾸기
+            i //= 2              # while 내부에서 i를 변경 -> 다음 조상 계속 검토
+
+    # 조상 노드에 저장된 정수의 합을 알아내기
+    while last > 0:
+        result += tree[last]
+        last //= 2
+
+
+    # print(f'정리한 tree:: {tree}')
+    print(f'#{tc} {result}')
+
+    '''
+    
+            7
+        2       5
+      3   4    6
+    ---------------------
+            2
+        5       7
+      3   4    6
+
+    
+    '''
+
--- 5178_sum_of_node.py
+++ 5178_sum_of_node.py
@@ -4,17 +4,20 @@ import sys
 sys.stdin = open("5178_input.txt")
 
 
-# def find_key(s, L): # s는 현재 탐색 노드(마지막 노드부터 시작)/ L은 찾아야 하는 노드 번호
-#     s//=2
-#     # L 노드에 값이 들어 있다면 그 값을 출력
-#     if heap[L]:
-#         return heap[L]
-#     # L 노드에 값이 들어 있지 않다면 = 아직 빈 값이라면
-#     else:
-#         if s > 0:
-#             if not heap[s] :
-#                 heap[s] = heap[s*2] + heap[s*2+1]
-#                 find_key(s, L)
+def find_key(s, L): # s는 현재 탐색 노드(마지막 노드부터 시작)/ L은 찾아야 하는 노드 번호
+    # L 노드에 값이 들어 있다면 그 값을 출력
+    if s < L:
+        return heap[L]
+    # L 노드에 값이 들어 있지 않다면 = 아직 빈 값이라면
+    else:
+        if heap[s] == 0:
+            if s*2+1 <= N:
+                heap[s] = heap[s * 2] + heap[s*2+1]
+            else:
+                heap[s] = heap[s * 2]
+        s -= 1
+        return find_key(s, L)
+
 
 
 
@@ -26,13 +29,6 @@ for tc in range(1, T+1):
         leaf, key = map(int, input().split())
         heap[leaf] = key
 
-    search = N//2
-    print(heap)
-    while True:
-        # 탐색하는 search 번호가 L일때 멈춰
-        if search == L:
-            break
-        else:
-            pass
+    search = N-M
 
-    # find_key(N, L)
+    print(f'#{tc} {find_key(search, L)}')
