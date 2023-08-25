f7d8671a00a986ea65c841959ac261ad63c432f9
jinyoooooooung <oaoa9n@gmail.com>
2023-08-25 10:47:24 +0900

    230825

--- 5185.py
+++ 5185.py
@@ -8,11 +8,16 @@ for tc in range(1, T+1):
     N, num = input().split()
     trans = {'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15}
     result = ''
-    for x in num:
+    for x in num[::-1]:
         if x.isalpha():
-            trans[x]%2
-            pass
+            x = trans[x]
+            for _ in range(4):
+                result = str(x % 2) + result
+                x//=2
         else:
-            # ten += int(x)
-            pass
-    print(f'ten:: ')
+            x = int(x)
+            for _ in range(4):
+                result = str(x % 2) + result
+                x //= 2
+
+    print(f'#{tc} {result}')
